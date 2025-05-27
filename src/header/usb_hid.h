ee36d4dcf2aa4b8c799d08d9fd1a6281ddb90eb7
alexander <alexanderattisani@gmail.com>
2025-05-27 22:00:53 +0200

    doc: add description

--- src/header/usb_hid.h
+++ src/header/usb_hid.h
@@ -1,3 +1,13 @@
+/**
+ * @file usb_hid.h
+ * @brief Header file for USB HID functionality.
+ * This file defines the HIDConfig structure and declares functions for starting USB HID communication and listing USB devices.
+ * @note This project uses the C17 language standard version.
+ * @note Ensure that the hidapi library is linked when compiling this code.
+ * @note The hidapi library must be installed on your system to use this functionality.
+ * @note resources: https://github.com/libusb/hidapi?tab=readme-ov-file#what-does-the-api-look-like
+ */
+
 #ifndef USB_HID_H
 #define USB_HID_H
 
